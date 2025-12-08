19585b44908a5457843eda02800d0898bcdbc7b5
JotaPeans <jpfontesferreira1@gmail.com>
2025-12-07 21:42:44 -0300

    update spring boot main method, feat secure

--- apresentacao/src/main/java/org/checkpoint/CheckpointApplication.java
+++ apresentacao/src/main/java/org/checkpoint/CheckpointApplication.java
@@ -1,11 +1,22 @@
 package org.checkpoint;
 
+import org.checkpoint.dominio.autenticacao.Autenticacao;
 import org.checkpoint.dominio.autenticacao.AutenticacaoTemplate;
+import org.checkpoint.dominio.comentario.ComentarioRepositorio;
+import org.checkpoint.dominio.comentario.ComentarioServico;
+import org.checkpoint.dominio.diario.DiarioRepositorio;
+import org.checkpoint.dominio.diario.DiarioServico;
+import org.checkpoint.dominio.email.EmailStrategy;
+import org.checkpoint.dominio.email.EmailServico;
+import org.checkpoint.dominio.email.VerificacaoEmailRepositorio;
 import org.checkpoint.dominio.jogo.JogoRepositorio;
 import org.checkpoint.dominio.jogo.JogoServico;
 import org.checkpoint.dominio.lista.ListaJogosRepositorio;
 import org.checkpoint.dominio.lista.ListaServico;
 import org.checkpoint.dominio.user.UserRepositorio;
+import org.checkpoint.dominio.user.UserServico;
+import org.checkpoint.persistencia.jpa.autenticacao.AutenticacaoImpl;
+import org.checkpoint.persistencia.jpa.email.ResendStrategy;
 import org.checkpoint.utils.AuthUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -25,8 +36,35 @@ public class CheckpointApplication {
     }
 
     @Bean
+    EmailStrategy resendStrategy() {
+        return new ResendStrategy();
+    }
+
+    @Bean
+    EmailServico emailServico(VerificacaoEmailRepositorio repositorio) {
+        return new EmailServico(repositorio, resendStrategy());
+    }
+
+    @Bean
+    DiarioServico diarioServico(DiarioRepositorio repositorio) {
+        return new DiarioServico(repositorio);
+    }
+
+    @Bean
+    UserServico userServico(UserRepositorio repositorio, DiarioRepositorio diarioRepositorio,
+                            VerificacaoEmailRepositorio verificacaoEmailRepositorio, Autenticacao autenticacao) {
+        return new UserServico(repositorio, diarioRepositorio,
+                new EmailServico(verificacaoEmailRepositorio, resendStrategy()), autenticacao);
+    }
+
+    @Bean
     JogoServico jogoServico(JogoRepositorio jogoRepositorio) {
         return new JogoServico(jogoRepositorio);
+    }
+
+    @Bean
+    ComentarioServico comentarioServico(ComentarioRepositorio comentarioRepositorio) {
+        return new ComentarioServico(comentarioRepositorio);
     }
 
     @Bean
---
+++ apresentacao/src/main/java/org/checkpoint/config/SecurityConfigurations.java
@@ -0,0 +1,47 @@
+package org.checkpoint.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return  httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
+
+
